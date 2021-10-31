d7b26cdedc7f55ba668ece2aacf6f07e0519e05c
ilkkaluu <xilehx@gmail.com>
2021-10-31 23:54:41 +0200

    edit

--- labyrintti/src/main/java/util/KPiste.java
+++ labyrintti/src/main/java/util/KPiste.java
@@ -9,9 +9,9 @@ public class KPiste {
     public boolean kayttoTrue;
 
     public KPiste(int s, int p[]) {
-        solu = s; //Seed Instanciation for kruskal algorithm, can use this.seed for easier readability but oh well
-        sijainti[0] = p[0];  //Position Instanciation
-        sijainti[1] = p[1];  //For easier getting
+        solu = s; 
+        sijainti[0] = p[0];  
+        sijainti[1] = p[1];
         kayttoTrue = true;
     }
 
