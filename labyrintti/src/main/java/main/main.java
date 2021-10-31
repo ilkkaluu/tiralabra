92587345ab0b8050e7754639a93cce59bf1575b1
ilkkaluu <62293652+ilkkaluu@users.noreply.github.com>
2021-10-31 22:16:12 +0200

    lisäys

---
+++ labyrintti/src/main/java/main/main.java
@@ -0,0 +1,13 @@
+package main;
+import util.RPrim;
+import util.RKruskal;
+
+class Main {
+
+    public static void main(String[] args) {
+        RPrim.suorita(20, 40);
+        RKruskal generator = new RKruskal(40); //input the size here
+        generator.uudestaan();
+        generator.tulosta();
+    }
+}
