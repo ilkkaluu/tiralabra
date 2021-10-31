a4f62f8db29c823a5aa2cf93f4d850dd36216df8
ilkkaluu <62293652+ilkkaluu@users.noreply.github.com>
2021-10-31 22:49:54 +0200

    lisäys

---
+++ labyrintti/src/main/java/main/main.java
@@ -0,0 +1,31 @@
+package main;
+
+import util.RPrim;
+import util.RKruskal;
+
+class Main {
+
+    public static void main(String[] args) {
+        long pAloitusaika = System.nanoTime();
+
+        RPrim.suorita(50, 50);      //Primin labyrintin koko, säädä arvoja halutun kokoiseksi tasaneliöksi.
+
+        long pLopetusaika = System.nanoTime();
+        long pKokonaisaika = (pLopetusaika - pAloitusaika) / 1000000;
+
+        System.out.println("");
+        System.out.println("Primissä aikaa kului: " + pKokonaisaika + " ms");
+        
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+
+        long kAloitusaika = System.nanoTime();
+        
+        RKruskal kruskalLabyrintti = new RKruskal(50); //Kruskalin labyrintin koko, säädä labyrintin kokoa
+        kruskalLabyrintti.uudestaan();
+        kruskalLabyrintti.tulosta();
+        
+        long kLopetusaika = System.nanoTime();
+        long kKokonaisaika = (kLopetusaika - kAloitusaika) / 1000000;
+        System.out.println("Kruskalissa aikaa kului: " + kKokonaisaika + " ms");
+    }
+}
